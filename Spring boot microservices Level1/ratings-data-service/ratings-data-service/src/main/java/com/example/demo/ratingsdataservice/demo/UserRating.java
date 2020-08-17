4ddac29e13d70d8d1f3df72c64d074ad812a8627
indhu-github <indhusworkmail@gmail.com>
2020-08-17 17:32:55 +0530

    updated upto level 1

---
+++ Spring boot microservices Level1/ratings-data-service/ratings-data-service/src/main/java/com/example/demo/ratingsdataservice/demo/UserRating.java
@@ -0,0 +1,16 @@
+package com.example.demo.ratingsdataservice.demo;
+
+import java.util.List;
+
+public class UserRating {
+	private List<Rating> userRating;
+
+	public List<Rating> getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(List<Rating> userRating) {
+		this.userRating = userRating;
+	}
+	
+}
